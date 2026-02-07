9be77dc30ec2fb35b93658cede099e713c8cf0fd
Brandon Casey <brandon@lagniappeit.com>
2026-02-07 09:27:34 -0600

    fixing user creation

--- factorio-lxc.sh
+++ factorio-lxc.sh
@@ -54,11 +54,13 @@ msg_info "Customizing Container: Finalizing"
 
 $STD mkdir -p /opt/factorio/saves /opt/factorio/mods /opt/factorio_backups
 
+$STD getent group | grep factorio || echo "Group 'factorio' does not exist yet"
+$STD getent passwd | grep factorio || echo "User 'factorio' does not exist yet"
 #$STD adduser --system --group --home /opt/factorio --no-create-home factorio
 #$STD groupadd factorio 2>/dev/null || true
 #$STD useradd -g factorio -d /opt/factorio -s /bin/bash factorio
-$STD useradd factorio
-$STD usermod -aG factorio factorio
+#$STD useradd factorio
+#$STD usermod -aG factorio factorio
 $STD chown -R factorio:factorio /opt/factorio
 
 $STD cp /opt/factorio/data/server-settings.example.json /opt/factorio/data/server-settings.json
